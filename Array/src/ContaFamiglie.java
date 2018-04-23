4830cf14f189c2f00bbfdebf73dccb024692c1b8
FABIO <FABIO@DESKTOP-TUNODU8>
2018-04-23 11:56:39 +0200

    Change

--- Array/src/ContaFamiglie.java
+++ Array/src/ContaFamiglie.java
@@ -3,38 +3,36 @@ import java.util.Scanner;
 public class ContaFamiglie {
 
 	public static void main(String[] args) {
-		int k; //rappresenta un intero che indica quanti valori ci sono nell' array
-		int valoreReddito;
-		double maxReddito;
-		double calcoloReddito=0;
-		int contaRedditoInferiore=0;
 		Scanner tastiera= new Scanner(System.in);
-		System.out.println("Quanti redditi devono essere inseriti?");
-		k= tastiera.nextInt();
-		double[] reddito= new double[k];
-		maxReddito=0;
+		System.out.println("Inserisci il numero di redditi che vuoi definire:");
+		int k=tastiera.nextInt();
+		double[] reddito=new double[k];
+		redditoFamiglia(reddito, k);
+		}
+	
+	
+	public static void redditoFamiglia(double[]reddito, int k){
+		Scanner tastiera= new Scanner(System.in);
+		double maxReddito=0;
 		
-		for(int i=0; i<reddito.length;i++){
-			System.out.println("Inserisci il reddito della "+i+" famiglia");
-			valoreReddito= tastiera.nextInt();
-			reddito[i]=valoreReddito;
-			
-			if (i==0)
-				maxReddito=reddito[0]; 
-			
-			if(reddito[i]>maxReddito)
+		for(int i=0;i<reddito.length;i++){
+			System.out.println("Inserisci il reddito della famiglia "+i);
+			reddito[i]=tastiera.nextDouble();
+			if(maxReddito<reddito[i])  //Definisco il reddito massimo
 				maxReddito=reddito[i];
 		}
-		
+		System.out.println();
 		System.out.println("il reddito max e': "+maxReddito);
-		calcoloReddito= (maxReddito*90)/100; //reddito inferiore fino al 10% del reddito massimo
 		
+		double calcoloReddito;
+		int redditoInferiore=0;
+		calcoloReddito= (maxReddito*90)/100;   //reddito inferiore fino al 10% del reddito massimo
+		System.out.println("Il reddito inferiore fino al 10% rispetto al reddito massimo vale: "+calcoloReddito);
 		for(int j=0;j<reddito.length;j++)
 			if(reddito[j]<calcoloReddito)
-				contaRedditoInferiore++;
-		
-		System.out.println("Le famiglie con reddito inferiore fino al 10% rispetto al reddito massimo sono:"+ contaRedditoInferiore);
-		}
+				redditoInferiore++;
+		System.out.println("Le famiglie con reddito inferiore fino al 10% rispetto al reddito massimo sono:"+ redditoInferiore);
+	}
 }
 
 
