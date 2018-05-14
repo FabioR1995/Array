7d63c50e008a5cf5c38e39341cdd019419df70d4
FABIO <FABIO@DESKTOP-TUNODU8>
2018-05-14 14:18:40 +0200

    Change Test

--- Array/src/StrettamenteCrescente.java
+++ Array/src/StrettamenteCrescente.java
@@ -1,20 +1,6 @@
 import java.util.Scanner;
 
 public class StrettamenteCrescente {
-
-	public static void main(String[] args) {
-		Scanner tastiera= new Scanner(System.in);
-		double[] array=new double[4];
-		System.out.println("Inserisci un valore positivo");
-		array[0]=tastiera.nextDouble();
-		for(int i=1;i<array.length;i++){
-			System.out.println("Inserisci un valore positivo");
-			array[i]=tastiera.nextDouble();
-			System.out.println(array[i-1]+">"+array[i]+"?");
-			System.out.println(strettamenteCrescente(array));
-			System.out.println("========================================================");
-		}
-	}
 	
 	public static boolean strettamenteCrescente(double[]in){
 		boolean risposta = true;  //verifica universale
--- Array/src/StrettamenteCrescenteTest.java
+++ Array/src/StrettamenteCrescenteTest.java
@@ -13,18 +13,18 @@ public class StrettamenteCrescenteTest {
 	@Test
 	public void testStrettamenteCrescente() {
 		double[] giaOrdinati= {0,1,2,3,4,5};
-		assertEquals(StrettamenteCrescente.strettamenteCrescente(giaOrdinati),true);
+		assertTrue(StrettamenteCrescente.strettamenteCrescente(giaOrdinati));
 	}
 	
 	@Test
 	public void testNonSonoCrescenti(){
 		double[] nonSonoOrdinati= {0,3,2,1,4,5};
-		assertEquals(StrettamenteCrescente.strettamenteCrescente(nonSonoOrdinati),false);
+		assertFalse(StrettamenteCrescente.strettamenteCrescente(nonSonoOrdinati));
 	}
 	
 	@Test
 	public void tuttiUguali(){
 		double [] numeriTuttiUguali= {1,1,1,1,1};
-		assertEquals(StrettamenteCrescente.strettamenteCrescente(numeriTuttiUguali),false);
+		assertFalse(StrettamenteCrescente.strettamenteCrescente(numeriTuttiUguali));
 	}
 }
